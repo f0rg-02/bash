2792d4b60cc417da14d6ea929c7b6385102bd783
f0rg <62225064+f0rg-02@users.noreply.github.com>
2023-10-22 18:05:42 -0400

    Update setup_vagrant.sh with getopts

--- setup_vagrant.sh
+++ setup_vagrant.sh
@@ -1,9 +1,53 @@
 #!/usr/bin/env bash
 
+VAGRANT_VM=""
+
+usage() {  # Function: Print a help message.
+  echo "Usage: $0 [ -n VAGRANT VM BOX NAME ]" 1>&2
+  echo "---------------"
+  echo "Example: $0 -n ubuntu/trusty64 (as according to https://app.vagrantup.com/ubuntu/boxes/trusty64)"
+
+}
+
+exit_abnormal() { # Function: Exit with error.
+  usage
+  echo "---------------"
+  echo "Exiting!"
+  exit 1
+}
+
+# Check if args were inputted
+
+while getopts "n:h" opt
+do
+        case ${opt} in
+        n)
+                echo "Vagrant box name: ${OPTARG}"
+                VAGRANT_VM="${OPTARG}"
+                ;;
+        h)
+                echo "---------------"
+                exit_abnormal
+                ;;
+        esac
+done
+
+# Check if supplied arguments are empty or not
+
+if [[ -z "$VAGRANT_VM" ]]
+then
+        echo "[!] Not all required arugments were supplied"
+        echo "---------------"
+        exit_abnormal
+        exit
+fi
+
 echo "[*] Setting up vagrant box"
 
-DIR=$(echo "$1" | cut -d"/" -f2)
-cd $HOME && mkdir $DIR && cd $DIR
-vagrant init "$1" && vagrant up && vagrant suspend
+DIR=${VAGRANT_VM//[\/]/_}
 
-echo "[*] Done"
+mkdir "$DIR" && cd "$DIR" && {
+    vagrant init "$VAGRANT_VM"
+    vagrant up
+    vagrant suspend
+}
