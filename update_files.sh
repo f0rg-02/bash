5f1ca0c0d6535e5be0b2d01cf545538b077e2dbc
f0rg_02 <f0rg_02@protonmail.ch>
2024-01-03 22:32:31 -0500

    Added update_files.sh and didn't test it. I need to sleep.

---
+++ update_files.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+DSTDIR=""
+SRCDIR=""
+
+usage() {  # Function: Print a help message.
+  echo "Usage: $0 [ -s DIRECTORY TO BE COPIED ] [ -d WHERE TO COPY TO ] [ -h HELP ]" 1>&2
+}
+
+exit_abnormal() { # Function: Exit with error.
+  usage
+  echo "---------------"
+  echo "Exiting!"
+  exit 1
+}
+
+ while getopts "s:d:h" opt
+ do
+	 case ${opt} in
+		s)
+			SRCDIR="${OPTARG}"
+			;;
+		d)
+			DSTDIR="${OPTARG}"
+			;;
+		h)
+			exit_abnormal
+			;;
+	esac
+done
+
+# Check if the arguments were inputted
+if [[ -z "$SRCDIR" || -z "$DSTDIR" ]]
+then
+	echo "[!] Not all required arugments were supplied"
+	echo "---------------"
+	exit_abnormal
+	exit
+fi
+
+cp -Ruv $SRCDIR $DSTDIR
