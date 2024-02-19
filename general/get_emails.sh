7be2e8bab41eb516b2bfd35f1de1f7cd8e561dde
f0rg_02 <f0rg_02@protonmail.ch>
2024-02-18 20:11:20 -0500

    Organized by creating subfolders and put the scripts in their respective folders

--- general/get_emails.sh
+++ general/get_emails.sh
@@ -3,5 +3,8 @@
 #directory=$(`dirname "$0"`)
 #directory=$("$(dirname "${BASH_SOURCE[0]}")")
 #echo "$directory"
+
+# Wrote this when I first started writing scripts
+# Was supposed to be used with the breach compilation
 zcat $(dirname "${BASH_SOURCE[0]}")/**/**/*.gz | grep -a "$1" # either im stupid or this is harder than it should be to get the fucking directory of the script. baka. owo
 
